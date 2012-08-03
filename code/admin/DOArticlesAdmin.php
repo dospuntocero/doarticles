dbaaf1ce204c9eeae2f2d40b92c58bb40feb0b53
francisco arenas <francisco.arenas@dospuntocero.cl>
2012-08-03 18:33:27 -0400

    new sidebar icon

--- code/admin/DOArticlesAdmin.php
+++ code/admin/DOArticlesAdmin.php
@@ -1,6 +1,12 @@
 <?php 
+
 class DOArticlesAdmin extends ModelAdmin {
 	public static $managed_models = array('DOArticle'); // Can manage multiple models
 	static $url_segment = 'articles'; // Linked as /admin/products/
 	static $menu_title = 'Articles';
+	
+	function init(){///
+		parent::init();
+		Requirements::customCSS(".icon.icon-16.icon-doarticlesadmin { background: url('/DOArticles/images/articlesadmin.png'); }");
+	}
 }
