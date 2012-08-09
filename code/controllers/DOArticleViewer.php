ddb9483313d2b6233b64165352af31b7b475b052
francisco arenas <francisco.arenas@dospuntocero.cl>
2012-08-09 01:37:34 -0400

    adding functions to article viewer controller

--- code/controllers/DOArticleViewer.php
+++ code/controllers/DOArticleViewer.php
@@ -1,10 +1,10 @@
 <?php
-class DOArticleViewer extends Controller {
+class DOArticleViewer extends Page_Controller {
 	
+	public function Tags(){
+		return DOTag::get();
+	}
 
-	public static $allowed_actions = array (
-		'read'
-	);
 
 	function read(){
 
@@ -12,8 +12,40 @@ class DOArticleViewer extends Controller {
 
 		$article = DOArticle::get()->filter('URLSegment' , $pid)->First();
 		if($article){
-			return $this->customise(array('Article' => $article))->renderWith(array('DOArticleHolderPage_view','Page'));
+			return $this->customise(array('Article'=>$article))->renderWith(array("DOArticleHolderPage_view","Page"));
 		}
 		return $this->httpError(404);
 	}
+	
+	public function PaginatedArticles() {
+		$pages = $this->data()->DOArticles()->sort('Date DESC');
+		$list = new PaginatedList($pages, $this->request);
+		$list->setPageLength(5);
+		return $list;
+	}
+	
+  public function bytag() {
+      $tags = false;
+      $theTag = $this->request->param('ID');
+      // Debug::show($theTag);
+      if (is_numeric($theTag)) {
+          $theTag = (int) $theTag;
+          $tags = DOTag::get()->filter(array('ID'=>$theTag))->first()->Articles("DOArticle.ID IN (Select zz.DOArticleID FROM DOArticleHolderPage_DOArticles zz where zz.DOArticleHolderPageID = ".$this->data()->ID.")");
+      } else {
+          $tags = DOTag::get()->filter(array('Title'=>$theTag))->first()->Articles("DOArticle.ID IN (Select zz.DOArticleID FROM DOArticleHolderPage_DOArticles zz where zz.DOArticleHolderPageID = ".$this->data()->ID.")");
+      }
+			// Debug::dump($tags);
+      if ($tags) {
+          $list = new PaginatedList($tags, $this->request);
+          $list->setPageLength(5);
+      } else {
+          $list = false;
+      }
+
+      return $this->customise(array('PaginatedArticles'=>$list))->renderWith(array("DOArticleHolderPage","Page"));
+
+  }
+
+	
+	
 }
