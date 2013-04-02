2f58e79cf3ea8962c0fcab36e9970532ba5f7b05
francisco arenas <francisco.arenas@dospuntocero.cl>
2013-04-02 01:04:36 -0400

    small letteres

--- code/pages/DOArticlesCategoryHolderPage.php
+++ code/pages/DOArticlesCategoryHolderPage.php
@@ -1,24 +1,24 @@
 <?php
 
-class DOArticlesCategoryHolderPage extends Page {
+class doarticlesCategoryHolderPage extends Page {
 
 	static $singular_name = "Article Categories Holder";
-	static $icon = "DOArticles/images/files.png";
+	static $icon = "doarticles/images/files.png";
 	static $description = 'List of categories for adding articles';
 
-	static $allowed_children = array("DOArticlesCategoryPage"); // set to string "none" or array of classname(s)
-	static $default_child = "DOArticlesCategoryPage"; //one classname
+	static $allowed_children = array("doarticlesCategoryPage"); // set to string "none" or array of classname(s)
+	static $default_child = "doarticlesCategoryPage"; //one classname
 	static $default_parent = null; // NOTE: has to be a URL segment NOT a class name
 	static $can_be_root = true; //
 	static $hide_ancestor = null; //dont show ancestry class
 
 	public function canCreate($member = null) {
-		return DataList::create("DOArticlesCategoryHolderPage")->count() < 1;
+		return DataList::create("doarticlesCategoryHolderPage")->count() < 1;
 	}
 
 }
 
-class DOArticlesCategoryHolderPage_Controller extends DOArticleViewer {
+class doarticlesCategoryHolderPage_Controller extends DOArticleViewer {
 
 	//always redirect to the first category
 	function init(){
--- code/pages/DOArticlesCategoryPage.php
+++ code/pages/DOArticlesCategoryPage.php
@@ -2,7 +2,7 @@
 class DOArticlesCategoryPage extends Page {
 
 	static $singular_name = "Articles Category Page";
-	static $icon = "DOArticles/images/article-category.png";
+	static $icon = "doarticles/images/article-category.png";
 	static $allowed_children = array('DOArticlesCategoryPage'); // set to string "none" or array of classname(s)
 	static $default_child = "none"; //one classname
 	static $default_parent = null; // NOTE: has to be a URL segment NOT a class name
