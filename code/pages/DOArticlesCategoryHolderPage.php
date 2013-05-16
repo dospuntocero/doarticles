57f006ddbaaada250d70b7226c1acdfb617e76c2
francisco arenas <francisco.arenas@dospuntocero.cl>
2013-05-16 00:21:18 -0400

    fixed names

--- code/pages/DOArticlesCategoryHolderPage.php
+++ code/pages/DOArticlesCategoryHolderPage.php
@@ -1,24 +1,24 @@
 <?php
 
-class doarticlesCategoryHolderPage extends Page {
+class DOArticlesCategoryHolderPage extends Page {
 
 	static $singular_name = "Article Categories Holder";
 	static $icon = "doarticles/images/files.png";
 	static $description = 'List of categories for adding articles';
 
 	static $allowed_children = array("doarticlesCategoryPage"); // set to string "none" or array of classname(s)
-	static $default_child = "doarticlesCategoryPage"; //one classname
+	static $default_child = "DOArticlesCategoryPage"; //one classname
 	static $default_parent = null; // NOTE: has to be a URL segment NOT a class name
 	static $can_be_root = true; //
 	static $hide_ancestor = null; //dont show ancestry class
 
 	public function canCreate($member = null) {
-		return DataList::create("doarticlesCategoryHolderPage")->count() < 1;
+		return DataList::create("DOArticlesCategoryHolderPage")->count() < 1;
 	}
 
 }
 
-class doarticlesCategoryHolderPage_Controller extends DOArticleViewer {
+class DOArticlesCategoryHolderPage_Controller extends DOArticleViewer {
 
 	//always redirect to the first category
 	function init(){
