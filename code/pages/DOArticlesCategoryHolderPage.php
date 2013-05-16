d0f560159681f782f3e77056957eb01fee1bb55d
francisco arenas <francisco.arenas@dospuntocero.cl>
2013-05-16 00:25:10 -0400

    fixed names

--- code/pages/DOArticlesCategoryHolderPage.php
+++ code/pages/DOArticlesCategoryHolderPage.php
@@ -6,7 +6,7 @@ class DOArticlesCategoryHolderPage extends Page {
 	static $icon = "doarticles/images/files.png";
 	static $description = 'List of categories for adding articles';
 
-	static $allowed_children = array("doarticlesCategoryPage"); // set to string "none" or array of classname(s)
+	static $allowed_children = array("DOArticlesCategoryPage"); // set to string "none" or array of classname(s)
 	static $default_child = "DOArticlesCategoryPage"; //one classname
 	static $default_parent = null; // NOTE: has to be a URL segment NOT a class name
 	static $can_be_root = true; //
